58c1f38858bf9830a862acb4672672aa58d1106d
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-08 14:24:59 -0300

    package installation

--- challenge-2/app/Http/Controllers/Controller.php
+++ challenge-2/app/Http/Controllers/Controller.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Server(url="http://localhost:8100/api"),
+ * @OA\Info(title="Challenge 2", version="0.0.1")
+ *
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
