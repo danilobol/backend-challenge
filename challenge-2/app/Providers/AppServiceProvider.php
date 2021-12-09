fbe1f1502e7904a3595dffa90125d36f96d5ba9e
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-09 02:20:31 -0300

    Update AppServiceProvider.php

--- challenge-2/app/Providers/AppServiceProvider.php
+++ challenge-2/app/Providers/AppServiceProvider.php
@@ -2,13 +2,23 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\IGroupRepository;
+use App\Repositories\Contracts\IUserGroupRepository;
 use App\Repositories\Contracts\IUserRepository;
 use App\Repositories\Contracts\IUserRoleRepository;
+use App\Repositories\Group\GroupRepository;
 use App\Repositories\User\UserRepository;
+use App\Repositories\UserGroup\UserGroupRepository;
 use App\Repositories\UserRole\UserRoleRepository;
+use App\Services\Contracts\IGroupService;
+use App\Services\Contracts\ImLearnService;
+use App\Services\Contracts\IUserGroupService;
 use App\Services\Contracts\IUserRoleService;
 use App\Services\Contracts\IUserService;
+use App\Services\Group\GroupService;
+use App\Services\mLearn\mLearnService;
 use App\Services\User\UserService;
+use App\Services\UserGroup\UserGroupService;
 use App\Services\UserRole\UserRoleService;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
@@ -27,11 +37,17 @@ class AppServiceProvider extends ServiceProvider
         //services
         $this->app->bind(IUserService::class, UserService::class);
         $this->app->bind(IUserRoleService::class, UserRoleService::class);
+        $this->app->bind(ImLearnService::class, mLearnService::class);
+        $this->app->bind(IUserGroupService::class, UserGroupService::class);
+        $this->app->bind(IGroupService::class, GroupService::class);
 
 
         //repositories
-        $this->app->bind(IUserRoleRepository::class, UserRoleRepository::class);
         $this->app->bind(IUserRepository::class, UserRepository::class);
+        $this->app->bind(IUserRoleRepository::class, UserRoleRepository::class);
+        $this->app->bind(IUserGroupRepository::class, UserGroupRepository::class);
+        $this->app->bind(IGroupRepository::class, GroupRepository::class);
+
     }
 
     /**
