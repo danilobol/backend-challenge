6f5e6e62e4935d118ee373e2799c9007308faa2e
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-12 19:46:18 -0300

    Update ImLearnService.php

--- challenge-2/app/Services/Contracts/ImLearnService.php
+++ challenge-2/app/Services/Contracts/ImLearnService.php
@@ -3,13 +3,43 @@
 namespace App\Services\Contracts;
 
 interface ImLearnService {
-    public function registerUserWithMLearn(string $msisdn, string $name, string $password, string $access_level = 'free');
-    public function findUserWithMLearn(string $userId, string $msisdn);
-    public function editUserWithMLearn(string $userId, string $msisdn, string $name, string $password, string $access_level = 'free');
-    public function updateAccessLevelUserWithMLearn(string $userId);
-    public function downgradeAccessLevelUserWithMLearn(string $userId);
-    public function deleteUserWithMLearn(string $userId);
-    public function addUserToGroupWithMLearn(string $userId, string $groupId, string $groupTitle);
-    public function getUserGroupWithMLearn(string $userId);
-    public function deleteUserGroupWithMLearn(string $userId, string $groupId);
+    public function registerUserWithMLearn(
+        string $userId,
+        string $msisdn,
+        string $name,
+        string $password,
+        string $access_level = 'pro'
+    );
+    public function findUserWithMLearn(
+        string $userId,
+        string $msisdn
+    );
+    public function editUserWithMLearn(
+        string $userId,
+        string $msisdn,
+        string $name,
+        string $password,
+        string $access_level = 'free'
+    );
+    public function updateAccessLevelUserWithMLearn(
+        string $userId
+    );
+    public function downgradeAccessLevelUserWithMLearn(
+        string $userId
+    );
+    public function deleteUserWithMLearn(
+        string $userId
+    );
+    public function addUserToGroupWithMLearn(
+        string $userId,
+        string $groupId,
+        string $groupTitle
+    );
+    public function getUserGroupWithMLearn(
+        string $userId
+    );
+    public function deleteUserGroupWithMLearn(
+        string $userId,
+        string $groupId
+    );
 }
