c471ef71611792668d60b5376c30ee1b8ab85b06
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-12 19:46:34 -0300

    Update IUserService.php

--- challenge-2/app/Services/Contracts/IUserService.php
+++ challenge-2/app/Services/Contracts/IUserService.php
@@ -3,7 +3,7 @@
 namespace App\Services\Contracts;
 
 interface IUserService {
-    public function registerUser(string $msisdn, string $name, string $password, string $access_level = 'free');
+    public function registerUser(string $msisdn, string $name, string $password, string $access_level = 'pro');
     public function loginUser(string $msisdn, string $password);
     public function findUser(int $id);
 }
