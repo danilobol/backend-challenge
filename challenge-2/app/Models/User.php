57c785a18c677e15452e74fe0c99eabe8a0e8821
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-12 19:46:03 -0300

    Update User.php

--- challenge-2/app/Models/User.php
+++ challenge-2/app/Models/User.php
@@ -9,8 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable, MakeUuid, SoftDeletes;
 
@@ -42,4 +43,35 @@ class User extends Authenticatable
         'deleted_at'
     ];
 
+    /**
+     * @var mixed
+     */
+    private $role;
+    /**
+     * @var mixed
+     */
+    private $user_roles;
+
+    public function getRouteKeyName()
+    {
+        return 'hash_code';
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        $roles = UserRole::FindAccessRoles($this->id);
+        return [
+            'user' => [
+                'id' => $this->id,
+                'email' => $this->email,
+                'nickname' => $this->name
+            ],
+            'roles' => $roles
+        ];
+    }
 }
